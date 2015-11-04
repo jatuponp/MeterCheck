31fe1c803a2cf8a9d4580d2fd0b62aa2c75f21f8
jatuponp <jatuponp@gmail.com>
2015-11-04 16:55:07 +0700

    add sqllit helper

--- app/src/main/java/com/nkc/metercheck/SyncActivity.java
+++ app/src/main/java/com/nkc/metercheck/SyncActivity.java
@@ -1,8 +1,7 @@
 package com.nkc.metercheck;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -11,6 +10,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class SyncActivity extends AppCompatActivity {
+    private ProgressDialog pDialog;
+    private TextView txtStatus;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,24 +20,41 @@ public class SyncActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
 
-        final TextView txtStatus = (TextView) findViewById(R.id.textView);
+        txtStatus = (TextView) findViewById(R.id.textView);
+
         Button btnSync = (Button) findViewById(R.id.button);
         btnSync.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("aasldfasf", "onclick");
-                txtStatus.setText("test test");
+                Log.d("aasldfasf", txtStatus.getText().toString());
+                syncDatabase();
             }
         });
+    }
+
+    private void syncDatabase(){
+        pDialog.setMessage("Synchronize Database. Please wait...");
+        showDialog();
+
+//        txtStatus.append("Synchronize Table Room: OK.");
+//        txtStatus.append("Synchronize Table Meter: OK.");
+        //txtStatus.setText("");
+
+        hideDialog();
+    }
+
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
     }
 
 }
